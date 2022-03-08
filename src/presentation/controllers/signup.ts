f5ef7cd99d69b2b8cebb8e8e44c65552b8d91bd8
Euller Tavares <eullertav@gmail.com>
2022-03-08 08:06:01 -0300

    feat: ensure SignUpController return 400 if password confirmations fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -30,9 +30,12 @@ export class SignUpController implements Controller {
       }
 
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
-
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
+      }
+
+      if (httpRequest.body.passowrd !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
     } catch (error) {
       return serverError()
