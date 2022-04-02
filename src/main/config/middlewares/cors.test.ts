06b5da995406aa23a42f9b34f936b0b7dd217edd
Euller Tavares <eullertav@gmail.com>
2022-04-02 19:10:10 -0300

    test: ensure cors is enable

---
+++ src/main/config/middlewares/cors.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '../app'
+
+describe('CORS Middleware', () => {
+  test('Should enable CORS', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app).post('/test_cors').expect('access-control-allow-origin', '*')
+    await request(app).post('/test_cors').expect('access-control-allow-methods', '*')
+    await request(app).post('/test_cors').expect('access-control-allow-headers', '*')
+  })
+})
