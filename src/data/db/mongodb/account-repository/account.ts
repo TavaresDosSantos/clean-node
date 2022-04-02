2bbd40e5ce9e7bf04f0ce84943bc0af93c3d9b82
Euller Tavares <eullertav@gmail.com>
2022-04-02 18:36:55 -0300

    refactor: move mapping to mongo helper

--- src/data/db/mongodb/account-repository/account.ts
+++ src/data/db/mongodb/account-repository/account.ts
@@ -4,20 +4,14 @@ import { AddAccountRepository } from '../../../../data/protocols/add-account-rep
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
-  async add (accountData: AddAccountModel): Promise<AccountModel> {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
 
     const { insertedId: id } = result
     const accountById = await accountCollection
       .findOne({ _id: id })
-    const { _id, ...accountWithoutId } = accountById
-    const account = Object.assign(
-      {},
-      accountWithoutId,
-      { id: _id.toHexString() }
-    ) as AccountModel
 
-    return account
+    return MongoHelper.map(accountById)
   }
 }
--- src/data/db/mongodb/helpers/mongo-helper.ts
+++ src/data/db/mongodb/helpers/mongo-helper.ts
@@ -1,3 +1,4 @@
+import { AccountModel } from '../../../../domain/models/account'
 import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
@@ -13,5 +14,14 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign(
+      {},
+      collectionWithoutId,
+      { id: _id.toHexString() }
+    ) as AccountModel
   }
 }
