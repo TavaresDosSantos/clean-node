9a173695a9fe3f6c00556b8712d3e1b0fc323364
Euller Tavares <eullertav@gmail.com>
2022-04-18 12:24:21 -0300

    feat: ensure LogControllerDecorator returns the same  result of the controller

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,28 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../data/db/mongodb/account-repository/account'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 
-export const makeSignUpController = (): SignUpController => {
+class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+
+  constructor(controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = this.controller.handle(httpRequest)
+    return httpResponse
+  };
+}
+
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+
+  return new LogControllerDecorator(signUpController)
 }
