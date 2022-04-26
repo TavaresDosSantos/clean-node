295486702cee8b8b77e69ae45168889b73f93222
Euller Tavares <eullertav@gmail.com>
2022-04-26 13:17:38 -0300

    test: ensure LoginController return 200 if valid credentials provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,10 +1,8 @@
-import { rejects } from 'assert'
-import { resolve } from 'path'
 import {
   InvalidParamError,
   MissingParamError
 } from '../../../presentation/errors'
-import { badRequest, serverError, unauthorized } from '../../../presentation/helpers/http-helpers'
+import { badRequest, ok, serverError, unauthorized } from '../../../presentation/helpers/http-helpers'
 import { LoginController } from './login'
 import { Authentication, EmailValidator, HttpRequest } from './login-protocols'
 
@@ -123,5 +121,12 @@ describe('Login Controller', () => {
 
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 if valid credentials provided', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
   })
 })
