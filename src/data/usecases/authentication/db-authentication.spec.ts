cf72c14994467f4598409b8aebc487a8cfd7ee1a
Euller Tavares <eullertav@gmail.com>
2022-04-30 16:28:26 -0300

    refactor: move data layer interfaces to folders

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,6 +1,5 @@
-import { rejects } from 'assert'
 import { AuthenticationModel } from '../../../domain/usecases/authentication'
-import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../add-account/db-add-accoun-protocols'
 import { DbAuthentication } from './db-authentication'
 
