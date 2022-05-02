4717305781da06d3282cd961041cbb27a7a65b52
Euller Tavares <eullertav@gmail.com>
2022-05-02 14:10:29 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,7 +1,10 @@
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import request from 'supertest'
 import app from '../config/app'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 
+let accountCollection: Collection
 describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -12,7 +15,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -23,6 +26,21 @@ describe('Login Routes', () => {
         email: 'euller.tavares@mail.com',
         password: '123456',
         passwordConfirmation: '123456'
+      }).expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return an 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Rodrigo',
+        email: 'email@email.com',
+        password
+      })
+      await request(app).post('/api/login').send({
+        email: 'email@email.com',
+        password: '123'
       }).expect(200)
     })
   })
