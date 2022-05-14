f897ddb4f1b667153e8d43b5f3edd7fcd1664092
Euller Tavares <eullertav@gmail.com>
2022-05-14 16:21:22 -0300

    refactor: move sut creation to a factory method

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -3,7 +3,7 @@ import { AccessDeniedError } from '../../presentation/errors'
 import { forbidden } from '../../presentation/helpers/http/http-helpers'
 import { HttpRequest, HttpResponse, Middleware } from '../../presentation/protocols'
 
-export class AutheMiddleware implements Middleware {
+export class AuthMiddleware implements Middleware {
   constructor (
     private readonly loadAccountByToken: LoadAccountByToken
   ) { }
