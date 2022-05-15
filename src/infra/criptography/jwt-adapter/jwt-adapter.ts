fafd8823e30b96619f07d5df5672a7301c74f4b9
Euller Tavares <eullertav@gmail.com>
2022-05-15 14:47:03 -0300

    feat: ensure JwtAdapter return a token on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -3,15 +3,15 @@ import jwt from 'jsonwebtoken'
 import { Encrypter } from '../../../data/protocols/criptography/encrypter'
 
 export class JwtAdapter implements Encrypter, Decrypter {
-  constructor (private readonly secret: string) {}
+  constructor(private readonly secret: string) { }
 
-  async encrypt (value: string): Promise<string> {
+  async encrypt(value: string): Promise<string> {
     const accessToken = await jwt.sign({ id: value }, this.secret)
     return accessToken
   }
 
-  async decrypt (value: string): Promise<string> {
-    await jwt.verify(value, this.secret)
-    return null
+  async decrypt (token: string): Promise<string> {
+    const value: any = await jwt.verify(token, this.secret)
+    return value
   }
 }
